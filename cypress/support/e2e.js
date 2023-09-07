c84e711387538b8eb9e075cc88c6879318194d03
vikum1407 <vikum1407@gmail.com>
2023-09-07 20:48:31 +0530

    Added exceptions for fail and uncaught to the e2e.json file

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -18,3 +18,13 @@ import './commands'
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
+
+cy.on("fail", (err, runnable) =>{
+    console.log(err.message);
+    return false;
+});
+
+cy.on('uncaught:exception', (err, runnable) =>{
+    console.log(err.message)
+    return false;
+});
